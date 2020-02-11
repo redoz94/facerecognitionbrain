6e0e2003d09d0bcb18b1e1e40f6931b098b9bf3c
Azfar Bilal <azfarbilal@Azfars-MacBook-Pro.local>
2020-02-11 12:04:55 +0500

    deploy

---
+++ build/precache-manifest.a27d6b34c723db5444d9b29ba2d443a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10684d83ccc6a813a3a555540ea8ca76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e34514d379e2318ec40b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "55fc9d53a95313ab4690",
+    "url": "/static/css/main.b18b26ee.chunk.css"
+  },
+  {
+    "revision": "e34514d379e2318ec40b",
+    "url": "/static/js/2.07f5eed8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07f5eed8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55fc9d53a95313ab4690",
+    "url": "/static/js/main.9f368350.chunk.js"
+  },
+  {
+    "revision": "af15c0d4ce7075498753",
+    "url": "/static/js/runtime-main.1ab7c014.js"
+  }
+]);
